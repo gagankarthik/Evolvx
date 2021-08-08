9413846f97d7e8c5f769e4b7b71e660cd5212a53
Gagankarthik <52318664+gagankarthik@users.noreply.github.com>
2021-08-08 17:55:32 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/*-----------  nav bar --------------- */
+$(window).scroll(function(){
+    $('nav').toggleClass('scrolled', $(this).scrollTop ()> 520);
+    $('a').toggleClass('scrolled', $(this).scrollTop ()> 100);
+    });
+
+
